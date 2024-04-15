95e75b6aa5ed3a1f6a20d97551384ec23a387414
Dash <dhanushdpandian@gmail.com>
2024-04-15 22:42:11 +0530

    added dropdown

--- app.py
+++ app.py
@@ -20,12 +20,14 @@ phone.send_keys(dob)
 opt1_button=web.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div[1]/div[1]/label/div/div[2]/div/span')
 opt1_button.click()
 
-# select_dropdown=web.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[1]/span')
+select_dropdown=web.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[1]/span')
+select_dropdown.click()
+time.sleep(2)
+opt2_button=web.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[4]/span')
+opt2_button.click()
 # dropdown = Select(select_dropdown)
 # dropdown.select_by_visible_text('Option Text')  
 # select_dropdown.select_by_index(2)
-# opt2_button=web.find_element('xpath','/html/body/div/div[3]/form/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[4]/span')
-# opt2_button.click()
 # option_value = 'Option 2'
 # web.execute_script("arguments[0].value = '{}';".format(option_value), select_dropdown)
 
